f0ad0999d8c28b6f69bf7720378b03337b29be30
Lyubomishchenko Denis <lyubomishchenko@SPEECHPRO.COM>
2018-05-29 16:37:27 +0300

    initial commit

---
+++ cmake/segments-union-on-line/segments-union-on-line.cmake
@@ -0,0 +1,5 @@
+set(SOURCES_SEGMENTS_UNION_ON_LINE
+	${segments-union-on-line_SOURCE_DIR}/../../src/segments-union-on-line/main.cpp
+)
+
+add_executable(segments-union-on-line ${segments-union-on-line_SOURCE_DIR})
