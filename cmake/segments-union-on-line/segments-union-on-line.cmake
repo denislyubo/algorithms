cab0ef50243e3a8bed9d9039f775b31e22ff5873
denis <denis@localhost.localdomain>
2018-05-29 17:05:06 +0300

    added segments union len algorithm

--- cmake/segments-union-on-line/segments-union-on-line.cmake
+++ cmake/segments-union-on-line/segments-union-on-line.cmake
@@ -2,4 +2,13 @@ set(SOURCES_SEGMENTS_UNION_ON_LINE
 	${segments-union-on-line_SOURCE_DIR}/../../src/segments-union-on-line/main.cpp
 )
 
-add_executable(segments-union-on-line ${segments-union-on-line_SOURCE_DIR})
+add_executable(segments-union-on-line ${SOURCES_SEGMENTS_UNION_ON_LINE})
+
+if (UNIX)
+	# link Dll
+	set_target_properties(
+		segments-union-on-line
+		PROPERTIES
+			LINK_FLAGS "-lrt"
+    )
+endif(UNIX)
--- src/segments-union-on-line/main.cpp
+++ src/segments-union-on-line/main.cpp
@@ -1,4 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+unsigned segments_union_measure (const vector <pair <int,int> > & a)
+{
+	unsigned n = a.size();
+	vector <pair <int,bool> > x (n*2);
+	for (unsigned i=0; i<n; i++)
+	{
+		x[i*2] = make_pair (a[i].first, false);
+		x[i*2+1] = make_pair (a[i].second, true);
+	}
+
+	sort (x.begin(), x.end());
+
+	unsigned result = 0;
+	unsigned c = 0;
+	for (unsigned i=0; i<n*2; i++)
+	{
+		if (c && i)
+			result += unsigned (x[i].first - x[i-1].first);
+		if (x[i].second)
+			++c;
+		else
+			--c;
+	}
+	return result;
+}
+
+
 int main() {
+
+	vector<pair<int, int>> segments{make_pair(1,3), make_pair(2, 4)};
+
+	auto res = segments_union_measure(segments);
+
+	cout << "Res: " << res << endl;
 
 	return 0;
 }
